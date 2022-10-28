4e5633e00597ef4da3ffbb4c40cc9c2e2bfd2aed
jin <raquim47@naver.com>
2022-10-28 12:36:39 +0900

    Add default setting

---
+++ main.jsx
@@ -0,0 +1,15 @@
+import React, {
+  useState,
+  useRef,
+  useEffect,
+  useMemo,
+  useCallback,
+} from "react";
+
+const Main = () => {
+  return (
+    <>
+    </>
+  )
+}
+export default Main;
