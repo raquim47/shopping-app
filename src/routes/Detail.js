5e6cd270a870d412805ec1452151f6719d6cfeb7
jin <raquim47@naver.com>
2022-11-05 01:41:02 +0900

    Add detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -16,25 +16,31 @@ const Detail = ({setPage}) => {
   return (
     
     <div className={styles.detail}>
-      <section className={styles.detail_img}>
+      <section className={styles.detail__left}>
         <img src={detailInfo.url}></img>
       </section>
-      <section className={styles.detail_order}>
-        <form>
+      <section className={styles.detail__right}>
+        <form className={styles.order_form}>
           <h3>{detailInfo.title}</h3>
-          <strong>$ {detailInfo.price}</strong>
-          <div className={styles.size}>
-            <h4>Size</h4>
+          {/* <div className={styles.order_form__price}>
+            <h4>price</h4>
+            <span>$ {detailInfo.price}</span>
+          </div> */}
+          <div className={styles.order_form__option}>
+            <h4>size</h4>
             <ul>
               {detailInfo.size && detailInfo.size.map(size => <li>{size}</li>)}
-              <li></li>
             </ul>
           </div>
-          <div className={styles.count}>
-            <h4>COUNT</h4>
-            <input type="number" />
+          <div className={styles.order_form__option}>
+            <h4>count</h4>
+            <input className={styles.count_input} type="number" />
           </div>
-          <button>Add Cart</button>
+          <div className={styles.order_form__option}>
+            <h4>price</h4>
+            <span>$ {detailInfo.price}</span>
+          </div>
+          <button type="submit" className={styles.order_form__submit_btn}>Add Cart</button>
         </form>
       </section>
     </div>
